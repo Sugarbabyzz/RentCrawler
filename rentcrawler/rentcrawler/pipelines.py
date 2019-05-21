5cecb19977fe5ace19b2ac9eb25d6cfb873c0007
Sugarbabyzz <406857586@qq.com>
2019-05-21 22:55:28 +0800

    Update pipelines.py

--- rentcrawler/rentcrawler/pipelines.py
+++ rentcrawler/rentcrawler/pipelines.py
@@ -42,7 +42,7 @@ class DuplicatesPipeline(object):
     def __init__(self):
         self.ids_seen = set()
     def process_item(self, item, spider):
-        if item['title'] in self.ids_seen:
+        if item['url'] in self.ids_seen:
             raise DropItem("Duplicate item found: %s" % item)
         else:
             self.ids_seen.add(item['title'])
