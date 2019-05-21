d70649c23081d0b17c6d4e51a744583ca9ce81e3
Sugarbabyzz <406857586@qq.com>
2019-05-21 20:30:42 +0800

    Update middlewares.py

--- rentcrawler/rentcrawler/middlewares.py
+++ rentcrawler/rentcrawler/middlewares.py
@@ -1,6 +1,11 @@
 # 设置随机的USER-AGENT
 import random
+import logging
+import requests
 
+'''
+    User-Agent
+'''
 class RandomUserAgentMiddleware():
     def __init__(self):
         self.user_agents = [
@@ -15,3 +20,37 @@ class RandomUserAgentMiddleware():
     def process_response(self, request, response, spider):
         response.status = 201
         return response
+
+'''
+    代理池
+'''
+class ProxyMiddleware():
+    def __init__(self, proxy_url):
+        self.logger = logging.getLogger(__name__)
+        self.proxy_url = proxy_url
+
+    def get_random_proxy(self):
+        try:
+            response = requests.get(self.proxy_url)
+            if response.status_code == 200:
+                proxy = response.text
+                return proxy
+        except requests.ConnectionError:
+            return False
+
+    def process_request(self, request, spider):
+        if request.meta.get('retry_times'):
+            proxy = self.get_random_proxy()
+            if proxy:
+                uri = 'https://{proxy}'.format(proxy=proxy)
+                self.logger.debug('使用代理 ' + proxy)
+                request.meta['proxy'] = uri
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        settings = crawler.settings
+        return cls(
+            proxy_url=settings.get('PROXY_URL')
+        )
+
+
