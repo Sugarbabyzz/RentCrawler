d294b42f21874132d71186378616b1851d8aa77a
Sugarbabyzz <406857586@qq.com>
2019-05-25 10:08:15 +0800

    Update items.py

--- rentcrawler/rentcrawler/items.py
+++ rentcrawler/rentcrawler/items.py
@@ -1,13 +1,11 @@
 from scrapy import Field, Item
 
 
-class LianjiaItem(Item):
-
-    # define the fields for your item here.
+class HouseItem(Item):
 
     house_city = Field()            # 所属城市
     house_title = Field()           # 房源信息标题
-    house_location = Field()      # 城市城区
+    house_location = Field()        # 城市城区
     house_lnglat = Field()          # 经纬度
     house_size = Field()            # 面积
     house_orient = Field()          # 朝向
@@ -16,6 +14,7 @@ class LianjiaItem(Item):
     house_price = Field()           # 价钱
     house_image = Field()           # 照片
     house_url = Field()             # 链接
+    house_refer = Field()             # 来源
 
 
 
