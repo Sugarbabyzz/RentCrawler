90f5408c7ebd0fac9bfec3afbb6ffd4d5b4e4c31
Sugarbabyzz <406857586@qq.com>
2019-05-25 10:08:21 +0800

    Update middlewares.py

--- rentcrawler/rentcrawler/middlewares.py
+++ rentcrawler/rentcrawler/middlewares.py
@@ -13,9 +13,18 @@ class RandomUserAgentMiddleware():
             'Mozilla/5.0 (X11; Ubantu; Linux i686; rv:15.0) Gecko/20100101 Firefox/15.0.1',
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2',
         ]
+        # self.proxys = [
+        #     '178.128.56.18:3128',
+        #     '178.128.56.18:31',
+        #     '167.99.63.67:8888',
+        #     '183.173.110.231:1080',
+        #     '58.244.192.5:8080'
+        # ]
 
     def process_request(self, request, spider):
         request.headers['User-Agent'] = random.choice(self.user_agents)
+        # print('当前使用的User-Agent为：' + str(request.headers['User-Agent']))
+        # print('当前使用的Proxy为：' + str(random.choice(self.proxys)))
 
     def process_response(self, request, response, spider):
         response.status = 201
@@ -44,6 +53,7 @@ class ProxyMiddleware():
             if proxy:
                 uri = 'https://{proxy}'.format(proxy=proxy)
                 self.logger.debug('使用代理 ' + proxy)
+                print('正在使用代理：' + proxy)
                 request.meta['proxy'] = uri
 
     @classmethod
